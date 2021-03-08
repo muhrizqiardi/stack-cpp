c99e9d35da4d416d86a70a860c172f660593681e
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-03-08 20:15:10 +0700

    fixed semicolon

--- main.cpp
+++ main.cpp
@@ -6,5 +6,5 @@ main() {
     std::cout << "()[]{()} is balanced is " << (check("()[]{()}") ? "true" : "false") << "\n"; // Will output "true"
     std::cout << "[(1+1]*2+3) is balanced is " << (check("[(1+1]*2+3)") ? "true" : "false") << "\n"; // Will output "false"
     std::cout << "[(x+y)*(3+z)]+(2*x) is balanced is " << (check("[(x+y)*(3+z)]+(2*x)") ? "true" : "false") << "\n"; // Will output "true"
-    std::cout << "\nInfix, postfix, and prefix: \n"
+    std::cout << "\nInfix, postfix, and prefix: \n";
 }
