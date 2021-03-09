29802d2457818a757a648b7d74a725be72ad0da2
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-03-09 07:55:33 +0700

    Infix Converter almost done

--- infixConverter.h
+++ infixConverter.h
@@ -1,11 +1,105 @@
 #include <iostream>
 
-std::string infixToPostfix(std::string exp)
+// Checks if a character is an operator
+bool oprCheck(char x)
 {
-    
-    for (int i = 0; i < exp.length(); i++)
+    switch (x)
     {
-        
-        
+    case '*':
+    case '/':
+    case '+':
+    case '-':
+        return true;
+        break;
+    default:
+        return false;
+        break;
     }
+    return false;
+}
+
+// Bracket check
+
+int bracketCheck(char x) {
+    switch (x)
+    {
+    case '{':
+    case '(':
+        return 1;
+        break;
+    case '}':
+    case ')':
+        return 2;
+        break;
+    default:
+        return 0;
+        break;
+    }
+    return 0;
+}
+
+// Precedence check
+int precedenceCheck(char opr)
+{
+    switch (opr)
+    {
+    case '{':
+    case '(':
+    case '}':
+    case ')':
+        return 3;
+    case '*':
+    case '/':
+        return 2;
+    case '+':
+    case '-':
+        return 1;
+        break;
+    default:
+        return 0;
+        break;
+    }
+    return 0;
+}
+
+// Takes an argument of two operator character, returns true if the first char has higher precedence
+bool hasHigherPrecedence(char a, char b)
+{
+    return precedenceCheck(a) > precedenceCheck(b);
+}
+
+// Converts infix expression to postfix
+std::string infixToPostfix(std::string infixExp)
+{
+    CharStack stack;
+    std::string postfixExp = ""; // String var to store the postfix expression after converted
+    for (int i = 0; i < infixExp.length(); i++)
+    {
+        // If the current character is an operand
+        if (!oprCheck(infixExp[i]))
+        {
+            postfixExp.push_back(infixExp[i]);
+            postfixExp.push_back(' ');
+        }
+        // If the current character is an operator
+        else if (oprCheck(infixExp[i]))
+        {
+            while (!stack.isEmpty() && hasHigherPrecedence(stack.peek(false), infixExp[i]) && bracketCheck(infixExp[i]) == 0)
+            {
+                postfixExp.push_back(stack.pop());
+                postfixExp.push_back(' ');
+            }
+            stack.push(infixExp[i]);
+        }
+        // If the current character is not an operand or and operator
+        else if (infixExp[i] == ' ')
+            continue;
+    }
+
+    while (!stack.isEmpty())
+    {
+        postfixExp.push_back(stack.pop());
+        postfixExp.push_back(' ');
+    }
+    return postfixExp;
 }
--- main.cpp
+++ main.cpp
@@ -4,12 +4,16 @@
 #include "infixConverter.h"
 #include <iostream>
 
-main() {
+main()
+{
     std::cout << "Balanced parentheses check: \n";
-    std::cout << "()[]{()} is balanced is " << (check("()[]{()}") ? "true" : "false") << "\n"; // Will output "true"
-    std::cout << "[(1+1]*2+3) is balanced is " << (check("[(1+1]*2+3)") ? "true" : "false") << "\n"; // Will output "false"
+    std::cout << "()[]{()} is balanced is " << (check("()[]{()}") ? "true" : "false") << "\n";                       // Will output "true"
+    std::cout << "[(1+1]*2+3) is balanced is " << (check("[(1+1]*2+3)") ? "true" : "false") << "\n";                 // Will output "false"
     std::cout << "[(x+y)*(3+z)]+(2*x) is balanced is " << (check("[(x+y)*(3+z)]+(2*x)") ? "true" : "false") << "\n"; // Will output "true"
     std::cout << "[()()]() is balanced is " << (check("[()()]()") ? "true" : "false") << "\n";                       // Will output "true"
-    std::cout << "\nEvaluate postfix: \n"; 
+    std::cout << "\nEvaluate postfix: \n";
     std::cout << "\"2 3 * 5 4 * + 9 -\" is " << evaluatePostfix("2 3 * 5 4 * + 9 -") << "\n";
+    std::cout << "\nConvert postfix: \n";
+    std::cout << "\"2 * 3 + 5 * 4 - 9\" is " << infixToPostfix("2 * 3 + 5 * 4 - 9") << "\n";
+    std::cout << "\"3 * (2 + 3) * 2\" is " << infixToPostfix("3 * (2 + 3) * 2") << "\n";
 }
