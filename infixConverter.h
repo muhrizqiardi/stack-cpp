61a3e6d4f2520b23b3d20c7416c80d2c6595fe53
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-03-11 11:41:00 +0700

    Infix to postfix convertion done

--- infixConverter.h
+++ infixConverter.h
@@ -18,9 +18,10 @@ bool oprCheck(char x)
     return false;
 }
 
-// Bracket check
-
-int bracketCheck(char x) {
+// Parentheses check
+// If the argument is an opening parentheses char return 1, if it's a closing parentheses return 2, otherwise return 0
+int parCheck(char x)
+{
     switch (x)
     {
     case '{':
@@ -73,33 +74,76 @@ std::string infixToPostfix(std::string infixExp)
 {
     CharStack stack;
     std::string postfixExp = ""; // String var to store the postfix expression after converted
+
     for (int i = 0; i < infixExp.length(); i++)
     {
-        // If the current character is an operand
-        if (!oprCheck(infixExp[i]))
+
+        // Loop count logger
+        std::cout << "\nloop " << i << "\n";
+
+        // If the current character is an operand (not an operator or a parentheses)
+        if (!oprCheck(infixExp[i]) && parCheck(infixExp[i]) == 0)
         {
             postfixExp.push_back(infixExp[i]);
-            postfixExp.push_back(' ');
         }
+
         // If the current character is an operator
         else if (oprCheck(infixExp[i]))
         {
-            while (!stack.isEmpty() && hasHigherPrecedence(stack.peek(false), infixExp[i]) && bracketCheck(infixExp[i]) == 0)
+
+            // Pop the stack until the top element of the stack is a parentheses
+            while (!stack.isEmpty() && parCheck(stack.peek(false)) == 0 && hasHigherPrecedence(stack.peek(false), infixExp[i]))
             {
                 postfixExp.push_back(stack.pop());
-                postfixExp.push_back(' ');
             }
+
+            // Push the operator to the stack
             stack.push(infixExp[i]);
         }
+
+        // If the current character is an opening parentheses
+        else if (parCheck(infixExp[i]) == 1)
+        {
+            stack.push(infixExp[i]);
+        }
+
+        // If the current character is an closing parentheses
+        else if (parCheck(infixExp[i]) == 2)
+        {
+
+            // Pop the stack and push it to string until the top element of the stack is a parentheses
+            while (!stack.isEmpty() && parCheck(stack.peek(false)) == 0)
+            {
+                postfixExp.push_back(stack.pop());
+            }
+
+            // Remove the parentheses on the top of the stack
+            if (parCheck(stack.peek(false)) > 0) {
+                stack.pop();
+            }
+
+        }
+
         // If the current character is not an operand or and operator
         else if (infixExp[i] == ' ')
             continue;
     }
 
+    // Emptying the stack as the final step of the algorithm
     while (!stack.isEmpty())
     {
-        postfixExp.push_back(stack.pop());
-        postfixExp.push_back(' ');
+        if (parCheck(stack.peek(false)) > 0)
+        {
+            stack.pop();
+        }
+        else 
+        {
+            postfixExp.push_back(stack.pop());
+        }
+            
     }
     return postfixExp;
 }
+
+// Convert infix to prefix expressions
+std::string infixToPrefix();
